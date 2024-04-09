48caa6dfb463d52e6e490e794df20034854cf995
dynstat <vs15vivek@gmail.com>
2024-04-09 14:02:26 +0530

    testcase added

--- leetcode/leet_567.py
+++ leetcode/leet_567.py
@@ -56,6 +56,9 @@ def checkInclusion(s1: str, s2: str) -> bool:
 
 
 if __name__ == "__main__":
+    s1 = "ab"
+    s2 = "eidbaooo"
+    print(checkInclusion(s1, s2))
     s1 = "a"
     s2 = "ab"
     print(checkInclusion(s1, s2))
