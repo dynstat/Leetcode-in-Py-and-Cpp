6eccbe0e34e068d19ab2e58c3295b4919b4d0576
VIVEK SINGH <vs15vivek@gmail.com>
2025-01-05 13:33:36 +0530

    leetcode 1 - E - 2sum - PY

---
+++ leetcode/leet_1.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    seen = {}  # val -> index
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+    return []  # No solution found
+
+
+if __name__ == "__main__":
+    print(twoSum([2, 7, 11, 15], 9))
+    print(twoSum([3, 2, 4], 6))
+    print(twoSum([3, 3], 6))
